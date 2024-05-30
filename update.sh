6427d46d7b1c532453d5e4246fa8ab80df581281
Playnary <martin@playnary.de>
2024-05-30 12:26:38 +0200

    Update update.sh
    Added PHP version control, user.ini maintenance and integrity check

--- update.sh
+++ update.sh
@@ -3,7 +3,8 @@
 # - @copyright Copyright (c) 2020 René Gieling <github@dartcafe.de>
 # -
 # - @author René Gieling <github@dartcafe.de>
-# -
+# - @modified by Playnary <martin@playnary.de>
+
 # - @license GNU AGPL version 3 or any later version
 # -
 # - This program is free software: you can redistribute it and/or modify
@@ -19,102 +20,34 @@
 # - You should have received a copy of the GNU Affero General Public License
 # - along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-# Autoupdater 0.2 for nextcloud installation of all-inkl.com customers
+# Autoupdater 0.4 for nextcloud installation of all-inkl.com customers
 # This script
+# - @playnary added: uses customizable php version
 # - sets the memory_limit in .user.ini
+# - @playnary added: sets the upload_max_filesize and max_execution_time in .user.ini
 # - makes sure, missing indices and columns are added
 # - updates apps, if available
 # - updates the nextcloud instance
+# - @playnary added: checks file and cache integrity afterwards
 # - supports multiple instances in one account
-#
-# Make sure, that this script runs on your installation. It works for me.
-# Use it at your own risk
 
-function test_installations() {
-    if [[ ! -f ${installations} ]]; then
-        logger "- installations.txt missing"
-        exit 1
-    fi
-}
+# @Playnary Roadmap for future version:
+# - enabling opCache locally
+# - integration of a remove-backups script to delete the automatic backups after successful core upgrade
 
-function logger()
-{
-    local text="$1"
-    if [[ "${text}" != "" ]] ; then
-			echo -e "\e[33m${text}\e[0m"
-    fi
-}
+# HOW TO:
+# 1. fill in installations.txt
+# 2. Check all directories and PhP versions in this script below to make sure that this script runs on your installation. It worked for us.
+# 3. automate this script via cron-job or run it via ssh
+# ! Use it at your own risk !
 
-function occ_add_indices()
-{
-	# occ db:add-missing-indices and :add-missing-columns is called blind
-	logger "- run occ db:add-missing-indices"
-	php -d memory_limit=$php_memory_limit $nc_base/occ db:add-missing-indices
-	logger "- run occ db:add-missing-columns"
-	php -d memory_limit=$php_memory_limit $nc_base/occ db:add-missing-columns
-	logger "- run occ db:add-missing-primary-keys"
-	php -d memory_limit=$php_memory_limit $nc_base/occ db:add-missing-primary-keys
-	logger "- run occ -n db:convert-filecache-bigint"
-	php -d memory_limit=$php_memory_limit $nc_base/occ -n db:convert-filecache-bigint
-}
 
-function set_php_limit()
-{
-	# set memory_limit to $php_memory_limit, if no memory_limit is set
-	if grep -q "memory_limit" $nc_base/.user.ini; then
-		logger "- leave $nc_base/.user.ini untouched, memory_limit is already set"
-	else
-		echo "memory_limit=$php_memory_limit" >> $nc_base/.user.ini
-		logger "- $nc_base/.user.ini memory_limit=$php_memory_limit added"
-	fi
-}
-
-function occ_upgrade()
-{
-	# run occ upgrade to make sure, all apps are up to date
-	logger "- make sure everything is up to date before update"
-	logger "- run occ upgrade first"
-	php -d memory_limit=$php_memory_limit $nc_base/occ upgrade
-}
-
-function occ_update_check() {
-	logger "- run occ update:check"
-	if php -d memory_limit=$php_memory_limit $nc_base/occ update:check | grep -q "Everything up to date"; then
-	    logger "- No Updates available"
-		update_available=0
-	else
-		logger "- Updates available"
-		update_available=1
-	fi
-
-	if php -d memory_limit=$php_memory_limit $nc_base/occ update:check | grep "Get more information on how to update"; then
-		logger "- New Nextcloud version is available"
-		update_available=2
-	fi
-}
-
-function occ_app_update()
-{
-	logger "- start updating apps"
-	php -d memory_limit=$php_memory_limit $nc_base/occ app:update --all -n
-}
-
-function update_nc_version()
-{
-	logger "- start update Nextcloud version"
-	php -d memory_limit=$php_memory_limit $nc_base/updater/updater.phar -n
-}
-
-function allow_eval_patch()
-{
-	patch_file=$nc_base/lib/public/AppFramework/Http/ContentSecurityPolicy.php
-	logger "- patching $patch_file"
-	logger "- replace \e[1m\e[95mprotected \e[91m\$evalScriptAllowed \e[95m= \e[32mfalse \e[0mwith \e[1m\e[95mprotected \e[91m\$evalScriptAllowed \e[95m= \e[32mtrue"
-	sed -i 's/protected $evalScriptAllowed = false/protected $evalScriptAllowed = true/g' $patch_file
-}
 
 # init check variables
 update_available=0
+
+# define the php version
+php_vs="82"
 
 script_dir=$(dirname "$0")
 # get account directory from username (/www/htdocs/w000000 in the example above)
@@ -128,12 +61,170 @@ account_base="/www/htdocs/${USER//ssh-}"
 # then add "domain.com/nextcloud" to the installations.txt
 installations="$script_dir/installations.txt"
 
-# same as installations, but for patching for allowing JS eval
-# use this for dev systems, where you have to allow eval (i.e. for dev tools)
-alloweval="$script_dir/alloweval.txt"
+# define the php_memory_limit, max_execution_time and upload_max_filesize
+max_execution_time="800"
+upload_max_filesize="2048M"
+php_memory_limit="1024M"
 
-# define the php_memory_limit
-php_memory_limit="512M"
+
+function logger()
+{
+    local text="$1"
+    if [[ "${text}" != "" ]] ; then
+			echo -e "\e[33m${text}\e[0m"
+    fi
+}
+
+
+function test_installations() {
+    if [[ ! -f ${installations} ]]; then
+        logger "- installations.txt missing"
+        exit 1
+
+       else
+       	logger "- installations.txt found"
+    fi
+}
+
+
+function occ_add_indices()
+{
+	# occ db:add-missing-indices and :add-missing-columns is called blind
+	logger "- run occ db:add-missing-indices"
+	php$php_vs -d memory_limit=$php_memory_limit $nc_base/occ db:add-missing-indices
+	logger "- run occ db:add-missing-columns"
+	php$php_vs -d memory_limit=$php_memory_limit $nc_base/occ db:add-missing-columns
+	logger "- run occ db:add-missing-primary-keys"
+	php$php_vs -d memory_limit=$php_memory_limit $nc_base/occ db:add-missing-primary-keys
+	logger "- run occ -n db:convert-filecache-bigint"
+	php$php_vs -d memory_limit=$php_memory_limit $nc_base/occ -n db:convert-filecache-bigint	
+
+}
+
+function set_php_limit()
+{
+	# set memory_limit to $php_memory_limit, if no memory_limit is set
+	if grep -q "memory_limit" $nc_base/.user.ini; then
+		logger "- leave $nc_base/.user.ini untouched, memory_limit is already set"
+	else
+		echo "memory_limit=$php_memory_limit" >> $nc_base/.user.ini
+		logger "- $nc_base/.user.ini memory_limit=$php_memory_limit added"
+	fi
+}
+
+function set_uploadsize_executiontime()
+{
+	# set, if is not set
+	if grep -q "upload_max_filesize" $nc_base/.user.ini; then
+		logger "- leave $nc_base/.user.ini untouched, upload_max_filesize is already set"
+	else
+		echo "upload_max_filesize=$upload_max_filesize" >> $nc_base/.user.ini
+		logger "- $nc_base/.user.ini upload_max_filesize=$upload_max_filesize added"
+	fi
+
+	if grep -q "max_execution_time" $nc_base/.user.ini; then
+		logger "- leave $nc_base/.user.ini untouched, max_execution_time is already set"
+	else
+		echo "max_execution_time=$max_execution_time" >> $nc_base/.user.ini
+		logger "- $nc_base/.user.ini max_execution_time=$max_execution_time added"
+	fi
+}
+
+
+function occ_update_check() {
+	logger "- run occ update:check"
+	if php$php_vs -d memory_limit=$php_memory_limit $nc_base/occ update:check | grep -q "Everything up to date"; then
+	    logger "- NO app or core versions updates available"
+		update_available=0
+	else
+		logger "- Some updates found.."
+		update_available=1
+	fi
+
+	if php$php_vs -d memory_limit=$php_memory_limit $nc_base/occ update:check | grep "Get more information on how to update"; then
+		logger "- A new Nextcloud version is available."
+		update_available=2
+	fi
+}
+
+function occ_app_update()
+{
+	logger "- Start updating apps"
+	php$php_vs -d memory_limit=$php_memory_limit $nc_base/occ app:update --all -n
+}
+
+function update_nc_version()
+{
+	# run directly start updater.phar
+	logger "- Start update Nextcloud version directly"
+	php$php_vs -d memory_limit=$php_memory_limit $nc_base/updater/updater.phar -n
+}
+
+function occ_upgrade()
+{
+	# run occ upgrade / alternative way of starting general updater
+	logger "- Start Nextcloud OCC updater"
+	php$php_vs -d memory_limit=$php_memory_limit $nc_base/occ upgrade
+}
+
+
+function occ_files_scan()
+{
+	logger "- Scan app files and check cache integrity"
+	php$php_vs -d memory_limit=$php_memory_limit $nc_base/occ files:scan-app-data
+}
+
+
+function allow_eval_patch()
+{
+	patch_file=$nc_base/lib/public/AppFramework/Http/ContentSecurityPolicy.php
+	logger "- patching $patch_file"
+	logger "- replace \e[1m\e[95mprotected \e[91m\$evalScriptAllowed \e[95m= \e[32mfalse \e[0mwith \e[1m\e[95mprotected \e[91m\$evalScriptAllowed \e[95m= \e[32mtrue"
+	sed -i 's/protected $evalScriptAllowed = false/protected $evalScriptAllowed = true/g' $patch_file
+}
+
+
+# Not yet implemented:
+# function delete_update_backups ()
+# {
+# 	# Get the directory of the Backups
+# 	SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+# 	folderPath=$SCRIPT_DIR/../nextcloud.playnary.de/data/updater-ocpjqdtfke8c/backups/
+
+# 	# Display the backup folder content and size
+# 	echo " - Folder Content:"
+# 	ls -lA $folderPath
+# 	echo ""
+# 	echo "- Folder Size:" 
+# 	echo $(du -sh $folderPath)
+
+# 	# !Remove prompt for automated behaviour! 
+# 	# Prompt the user for input
+# 	echo -n "Do you want to proceed with deleting the update-backups? (y/n): "
+# 	read response
+
+# 	# Check the user's input
+# 	if [[ $response == "y" ]] ; then
+	 
+# 		# Delete all folders and files in the parent directory
+# 		if rm -r "$SCRIPT_DIR/../nextcloud.playnary.de/data/updater-ocpjqdtfke8c/backups/"; then
+# 			echo "- folder $SCRIPT_DIR succesfully DELETED"
+# 		else
+# 			echo "- could not find or delete folder $SCRIPT_DIR"
+# 		fi
+
+# 	else
+# 	  echo "Backup Deletion aborted."
+
+# 	fi
+
+# }
+
+
+#Script Runtime
+
+test_installations
+#logger "– Script running with \e[96m$installations in $account_base"
 
 while read install_dir; do
 	nc_base=$account_base/$install_dir
@@ -150,9 +241,8 @@ while read install_dir; do
 	chmod 744 $nc_base/occ
 
 	set_php_limit
-	occ_upgrade
+	set_uploadsize_executiontime
 	occ_add_indices
-	occ_app_update
 	occ_update_check
 
 	# assuming that occ update:check still reports the same strings
@@ -160,16 +250,26 @@ while read install_dir; do
 	# "update for" means there is an update for at least one app
 	# "Get more information on how to update" means, there is a Nextcloud update available
 	if [ "${update_available}" != "0" ] ; then
-		logger "- start updating apps"
+
+		logger " "
 		occ_app_update
+		logger " "
 
 		if [ "${update_available}" = "2" ] ; then
+
 			update_nc_version
-			occ_add_indices
+			# occ_upgrade # we are using the direct command above for now
+			
+			logger " "
+			logger "- Post-update treatment: Reset memory limits, upload sizes and executions times according to custom specs"
 			set_php_limit
+			set_uploadsize_executiontime
+			occ_files_scan
+
 		fi
 	fi
-done <$installations
+done < $installations
+
 
 while read install_dir; do
 	nc_base=$account_base/$install_dir
